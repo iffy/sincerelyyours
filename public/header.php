b0348fa83f720f4f8cfb1dbd849d0ba9e3599846
jebarllow <jebarlow@gmail.com>
2015-01-09 18:11:22 -0700

    change sincerely yours to my stories are in headers

--- public/header.php
+++ public/header.php
@@ -1,10 +1,24 @@
 <html>
   <head>
-    <title>Welcome to Sincerely Yours</title>
+    <title>My Stories Are</title>
     <link href="public/main.css" media="all" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <div id="header">
-      <h1>Welcome to Sincerely Yours</h1>
-    </div>
+    <section class="rw-wrapper">
+    <h2 class="rw-sentence">
+        <span>My Stories Are</span>
+        <div class="rw-words rw-words-1">
+            <span>funny</span>
+            <span>weird</span>
+            <span>necessary</span>
+            <span>important</span>
+            <span>classy</span>
+            <span>humbling</span>
+        </div>
+        <br />
+        
+    </h2>
+	</section>
+	</div>
     <div id="main">
