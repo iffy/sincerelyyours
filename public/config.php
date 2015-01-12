3593abcbb8e80314c40ba6fb8fedd6951c2b7e87
Matt Haggard <haggardii@gmail.com>
2015-01-12 12:17:53 -0700

    Don't break current use-case

--- public/config.php
+++ public/config.php
@@ -2,10 +2,10 @@
 
 //Database Constants
 
-defined('DB_SERVER') 	?null : define ("DB_SERVER",getenv('MYSQL_HOST'));
-defined('DB_USER') 		?null : define ("DB_USER",getenv('MYSQL_USER'));
-defined('DB_PASS') 		?null : define ("DB_PASS",getenv('MYSQL_PASSWORD'));
-defined('DB_NAME') 		?null : define ("DB_NAME",getenv('MYSQL_DATABASE'));
-defined('DB_PORT')      ?null : define ("DB_PORT",intval(getenv("MYSQL_PORT")));
+defined('DB_SERVER') 	?null : define ("DB_SERVER",getenv('MYSQL_HOST') ?: "localhost");
+defined('DB_USER') 		?null : define ("DB_USER",getenv('MYSQL_USER') ?: "root");
+defined('DB_PASS') 		?null : define ("DB_PASS",getenv('MYSQL_PASSWORD') ?: "remote99");
+defined('DB_NAME') 		?null : define ("DB_NAME",getenv('MYSQL_DATABASE') ?: "story");
+defined('DB_PORT')      ?null : define ("DB_PORT",getenv("MYSQL_PORT") ? intval(getenv("MYSQL_PORT")) : NULL);
 
 ?>
